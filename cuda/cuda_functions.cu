b5855ac713880f395cb2afb61cd02cff4be22086
Symela <sykom21@gmail.com>
2018-10-11 15:03:28 +0300

    filter for the outline of the picture

--- cuda/cuda_functions.cu
+++ cuda/cuda_functions.cu
@@ -16,16 +16,89 @@ __global__ void kernel_grey(uint8_t* start, uint8_t* end, int height, int width)
   size_t x = blockIdx.x * blockDim.x + threadIdx.x;
   size_t y = blockIdx.y * blockDim.y + threadIdx.y;
 
-  if (0 < x && x < height-1 && 0 < y && y < width-1) {
-    float new_val;
-    new_val = 0;
+  float new_val;
+  new_val = 0;
+
+  if (0 < x && x < height-1 && 0 < y && y < width-1) { // to eswteriko thw eikonas
     for (i = x-1, k = 0 ; i <= x+1 ; i++, k++) {
       for (j = y-1, l = 0 ; j <= y+1 ; j++, l++) {
-        new_val += src[width * i + j] * h[k][l] / 16.0;
+        new_val += start[width * i + j] * filter[k][l] / 16.0;
       }
     }
-    dst[width * x + y] = new_val;
+    end[width * x + y] = new_val;
 	}
+  else if ( x == 0 && y == 0 ) { // panw aristera gwnia
+    new_val = 5 * start[width * x + y];
+    for (i = x, k = 1 ; i <= x+1 ; i++, k++) {
+      for (j = y, l = 1 ; j <= y+1 ; j++, l++) {
+        new_val += start[width * i + j] * filter[k][l] / 16.0;
+      }
+    }
+    end[width * x + y] = new_val;
+  }
+  else if ( x == height-1 && y == 0 ) { // katw aristera gwnia
+    new_val = 5 * start[width * x + y];
+    for (i = x-1, k = 0 ; i <= x ; i++, k++) {
+      for (j = y, l = 1 ; j <= y+1 ; j++, l++) {
+        new_val += start[width * i + j] * filter[k][l] / 16.0;
+      }
+    }
+    end[width * x + y] = new_val;
+  }
+  else if ( x == 0 && y == width-1 ) { // panw deksia gwnia
+    new_val = 5 * start[width * x + y];
+    for (i = x, k = 1 ; i <= x+1 ; i++, k++) {
+      for (j = y-1, l = 0 ; j <= y ; j++, l++) {
+        new_val += start[width * i + j] * filter[k][l] / 16.0;
+      }
+    }
+    end[width * x + y] = new_val;
+  }
+  else if ( x == height-1 && y == width-1 ) { // katw deksia gwnia
+    new_val = 5 * start[width * x + y];
+    for (i = x-1, k = 0 ; i <= x ; i++, k++) {
+      for (j = y-1, l = 0 ; j <= y ; j++, l++) {
+        new_val += start[width * i + j] * filter[k][l] / 16.0;
+      }
+    }
+    end[width * x + y] = new_val;
+  }
+  else if ( x == 0 ) { // 1h grammh xwris tis akres
+    new_val = 3 * start[width * x + y];
+    for (i = x, k = 1 ; i <= x+1 ; i++, k++) {
+      for (j = y-1, l = 0 ; j <= y+1 ; j++, l++) {
+        new_val += start[width * i + j] * filter[k][l] / 16.0;
+      }
+    }
+    end[width * x + y] = new_val;
+  }
+  else if ( x == height-1 ) { // teleutaia grammh xwris tis akres
+    new_val = 3 * start[width * x + y];
+    for (i = x-1, k = 0 ; i <= x ; i++, k++) {
+      for (j = y-1, l = 0 ; j <= y+1 ; j++, l++) {
+        new_val += start[width * i + j] * filter[k][l] / 16.0;
+      }
+    }
+    end[width * x + y] = new_val;
+  }
+  else if ( y == 0 ) { // 1h sthlh xwris akres
+    new_val = 3 * start[width * x + y];
+    for (i = x-1, k = 0 ; i <= x+1 ; i++, k++) {
+      for (j = y, l = 1 ; j <= y+1 ; j++, l++) {
+        new_val += start[width * i + j] * filter[k][l] / 16.0;
+      }
+    }
+    end[width * x + y] = new_val;
+  }
+  else if ( y == width-1 ) { // teleutaia sthlh xwris akres
+    new_val = 3 * start[width * x + y];
+    for (i = x-1, k = 0 ; i <= x+1 ; i++, k++) {
+      for (j = y-1, l = 0 ; j <= y ; j++, l++) {
+        new_val += start[width * i + j] * filter[k][l] / 16.0;
+      }
+    }
+    end[width * x + y] = new_val;
+  }
 }
 
 
@@ -37,22 +110,185 @@ __global__ void kernel_rgb(uint8_t* start, uint8_t* end, int height, int width)
   size_t x = blockIdx.x * blockDim.x + threadIdx.x;
   size_t y = blockIdx.y * blockDim.y + threadIdx.y;
 
-  if (0 < x && x < height-1 && 0 < y && y < (3*width)-3) {
-    float new_val_red, new_val_blue, new_val_green;
-    new_val_red = 0;
-    new_val_blue = 0;
-    new_val_green = 0;
+  float new_val_red, new_val_blue, new_val_green;
+  new_val_red = 0;
+  new_val_blue = 0;
+  new_val_green = 0;
+
+  if (0 < x && x < height-1 && 0 < y && y < (3*width)-3) { // to eswteriko thw eikonas
     for (i = x-1, k = 0 ; i <= x+1 ; i++, k++) {
       for (j = (y*3)-3, l = 0 ; j <= (y*3)+3 ; j += 3, l++) {
-        new_val_red += src[(width*3) * i + j] * h[k][l] / 16.0;
-        new_val_green += src[(width*3) * i + j + 1] * h[k][l] / 16.0;
-        new_val_blue += src[(width*3) * i + j + 2] * h[k][l] / 16.0;
+        new_val_red += start[(width*3) * i + j] * filter[k][l] / 16.0;
+        new_val_green += start[(width*3) * i + j + 1] * filter[k][l] / 16.0;
+        new_val_blue += start[(width*3) * i + j + 2] * filter[k][l] / 16.0;
       }
     }
-    dst[(width*3) * x + (y*3)] = new_val_red;
-    dst[(width*3) * x + (y*3) + 1] = new_val_green;
-    dst[(width*3) * x + (y*3) + 2] = new_val_blue;
+    end[(width*3) * x + (y*3)] = new_val_red;
+    end[(width*3) * x + (y*3) + 1] = new_val_green;
+    end[(width*3) * x + (y*3) + 2] = new_val_blue;
 	}
+  else if ( x == 0 && y == 0 ) { // panw aristera gwnia
+    new_val_red = 5 * start[(width*3) * x + y];
+    new_val_green = 5 * start[(width*3) * x + y + 1];
+    new_val_blue = 5 * start[(width*3) * x + y + 2];
+
+    for (i = x, k = 1 ; i <= x+1 ; i++, k++) {
+      for (j = (y*3), l = 1 ; j <= (y*3)+3 ; j += 3, l++) {
+        new_val_red += start[(width*3) * i + j] * filter[k][l] / 16.0;
+        new_val_green += start[(width*3) * i + j + 1] * filter[k][l] / 16.0;
+        new_val_blue += start[(width*3) * i + j + 2] * filter[k][l] / 16.0;
+      }
+    }
+    end[(width*3) * x + (y*3)] = new_val_red;
+    end[(width*3) * x + (y*3) + 1] = new_val_green;
+    end[(width*3) * x + (y*3) + 2] = new_val_blue;
+  }
+  else if ( x == height-1 && y == 0 ) { // katw aristera gwnia
+    new_val_red = 5 * start[(width*3) * x + y];
+    new_val_green = 5 * start[(width*3) * x + y + 1];
+    new_val_blue = 5 * start[(width*3) * x + y + 2];
+
+    for (i = x-1, k = 0 ; i <= x ; i++, k++) {
+      for (j = (y*3), l = 1 ; j <= (y*3)+3 ; j += 3, l++) {
+        new_val_red += start[(width*3) * i + j] * filter[k][l] / 16.0;
+        new_val_green += start[(width*3) * i + j + 1] * filter[k][l] / 16.0;
+        new_val_blue += start[(width*3) * i + j + 2] * filter[k][l] / 16.0;
+      }
+    }
+    end[(width*3) * x + (y*3)] = new_val_red;
+    end[(width*3) * x + (y*3) + 1] = new_val_green;
+    end[(width*3) * x + (y*3) + 2] = new_val_blue;
+  }
+  else if ( x == 0 && y == (3*width)-3) ) { // panw deksia gwnia
+    new_val_red = 5 * start[(width*3) * x + y];
+    new_val_green = 5 * start[(width*3) * x + y + 1];
+    new_val_blue = 5 * start[(width*3) * x + y + 2];
+
+    for (i = x, k = 1 ; i <= x+1 ; i++, k++) {
+      for (j = (y*3)-3, l = 0 ; j <= (y*3) ; j += 3, l++) {
+        new_val_red += start[(width*3) * i + j] * filter[k][l] / 16.0;
+        new_val_green += start[(width*3) * i + j + 1] * filter[k][l] / 16.0;
+        new_val_blue += start[(width*3) * i + j + 2] * filter[k][l] / 16.0;
+      }
+    }
+    end[(width*3) * x + (y*3)] = new_val_red;
+    end[(width*3) * x + (y*3) + 1] = new_val_green;
+    end[(width*3) * x + (y*3) + 2] = new_val_blue;
+  }
+  else if ( x == height-1 && y == (3*width)-3) ) { // katw deksia gwnia
+    new_val_red = 5 * start[(width*3) * x + y];
+    new_val_green = 5 * start[(width*3) * x + y + 1];
+    new_val_blue = 5 * start[(width*3) * x + y + 2];
+
+    for (i = x-1, k = 0 ; i <= x ; i++, k++) {
+      for (j = (y*3)-3, l = 0 ; j <= (y*3) ; j += 3, l++) {
+        new_val_red += start[(width*3) * i + j] * filter[k][l] / 16.0;
+        new_val_green += start[(width*3) * i + j + 1] * filter[k][l] / 16.0;
+        new_val_blue += start[(width*3) * i + j + 2] * filter[k][l] / 16.0;
+      }
+    }
+    end[(width*3) * x + (y*3)] = new_val_red;
+    end[(width*3) * x + (y*3) + 1] = new_val_green;
+    end[(width*3) * x + (y*3) + 2] = new_val_blue;
+  }
+  else if ( x == 0 ) { // 1h grammh xwris tis akres
+    new_val_red = 3 * start[(width*3) * x + y];
+    new_val_green = 3 * start[(width*3) * x + y + 1];
+    new_val_blue = 3 * start[(width*3) * x + y + 2];
+
+    for (i = x, k = 1 ; i <= x+1 ; i++, k++) {
+      for (j = (y*3)-3, l = 0 ; j <= (y*3)+3 ; j += 3, l++) {
+        new_val_red += start[(width*3) * i + j] * filter[k][l] / 16.0;
+        new_val_green += start[(width*3) * i + j + 1] * filter[k][l] / 16.0;
+        new_val_blue += start[(width*3) * i + j + 2] * filter[k][l] / 16.0;
+      }
+    }
+    end[(width*3) * x + (y*3)] = new_val_red;
+    end[(width*3) * x + (y*3) + 1] = new_val_green;
+    end[(width*3) * x + (y*3) + 2] = new_val_blue;
+  }
+  else if ( x == height-1 ) { // teleutaia grammh xwris tis akres
+    new_val_red = 3 * start[(width*3) * x + y];
+    new_val_green = 3 * start[(width*3) * x + y + 1];
+    new_val_blue = 3 * start[(width*3) * x + y + 2];
+
+    for (i = x-1, k = 0 ; i <= x ; i++, k++) {
+      for (j = (y*3)-3, l = 0 ; j <= (y*3)+3 ; j += 3, l++) {
+        new_val_red += start[(width*3) * i + j] * filter[k][l] / 16.0;
+        new_val_green += start[(width*3) * i + j + 1] * filter[k][l] / 16.0;
+        new_val_blue += start[(width*3) * i + j + 2] * filter[k][l] / 16.0;
+      }
+    }
+    end[(width*3) * x + (y*3)] = new_val_red;
+    end[(width*3) * x + (y*3) + 1] = new_val_green;
+    end[(width*3) * x + (y*3) + 2] = new_val_blue;
+  }
+  else if ( y == 0 ) { // 1h sthlh xwris akres
+    new_val_red = 3 * start[(width*3) * x + y];
+    new_val_green = 3 * start[(width*3) * x + y + 1];
+    new_val_blue = 3 * start[(width*3) * x + y + 2];
+
+    for (i = x-1, k = 0 ; i <= x+1 ; i++, k++) {
+      for (j = (y*3), l = 1 ; j <= (y*3)+3 ; j += 3, l++) {
+        new_val_red += start[(width*3) * i + j] * filter[k][l] / 16.0;
+        new_val_green += start[(width*3) * i + j + 1] * filter[k][l] / 16.0;
+        new_val_blue += start[(width*3) * i + j + 2] * filter[k][l] / 16.0;
+      }
+    }
+    end[(width*3) * x + (y*3)] = new_val_red;
+    end[(width*3) * x + (y*3) + 1] = new_val_green;
+    end[(width*3) * x + (y*3) + 2] = new_val_blue;
+  }
+  else if ( y == (3*width)-3) ) { // teleutaia sthlh xwris akres
+    new_val_red = 3 * start[(width*3) * x + y];
+    new_val_green = 3 * start[(width*3) * x + y + 1];
+    new_val_blue = 3 * start[(width*3) * x + y + 2];
+
+    for (i = x-1, k = 0 ; i <= x+1 ; i++, k++) {
+      for (j = (y*3)-3, l = 0 ; j <= (y*3) ; j += 3, l++) {
+        new_val_red += start[(width*3) * i + j] * filter[k][l] / 16.0;
+        new_val_green += start[(width*3) * i + j + 1] * filter[k][l] / 16.0;
+        new_val_blue += start[(width*3) * i + j + 2] * filter[k][l] / 16.0;
+      }
+    }
+    end[(width*3) * x + (y*3)] = new_val_red;
+    end[(width*3) * x + (y*3) + 1] = new_val_green;
+    end[(width*3) * x + (y*3) + 2] = new_val_blue;
+  }
+}
+
+
+__global__ int kernel_grey_equal(uint8_t* start, uint8_t* end, int height, int width)
+{
+  size_t x = blockIdx.x * blockDim.x + threadIdx.x;
+  size_t y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  int i, j;
+
+  for (i = x; i <= height-1 ; i++) {
+    for (j = y; j <= width-1 ; j++) {
+      if(start[width * i + j] == end[width * i + j]) continue;
+      else return 0;
+    }
+  }
+  return 1;
+}
+
+
+__global__ int kernel_rgb_equal(uint8_t* start, uint8_t* end, int height, int width)
+{
+  size_t x = blockIdx.x * blockDim.x + threadIdx.x;
+  size_t y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  int i, j;
+
+  for (i = x; i <= height-1 ; i++) {
+    for (j = y; j <= (3*width)-3 ; j += 3) {
+      if(start[(width*3) * i + j] == end[(width*3) * i + j] && start[(width*3) * i + j + 1] == end[(width*3) * i + j + 1] && start[(width*3) * i + j + 2] == end[(width*3) * i + j + 2] ) continue;
+      else return 0;
+    }
+  }
+  return 1;
 }
 
 
@@ -97,10 +333,25 @@ extern "C" void filtering( uint8_t* table, int height, int width, int loops, cha
       kernel_rgb<<<dimGrid, dimBlock>>>(start, end, height, width);
     }
 
-    if( strcmp(start, end) == 0 )
+    if(strcmp(type, "GREY") == 0)
     {
-      flag_no_change = 1;
+      int gridX, gridY;
+      gridX = (height + blocksize - 1) / blocksize;
+      gridY = (width + blocksize - 1) / blocksize;
+      dim3 dimBlock(blocksize, blocksize);
+      dim3 dimGrid(gridX, gridY);
+      if(kernel_grey_equal<<<dimGrid, dimBlock>>>(start, end, height, width) == 0) flag_no_change = 1;
     }
+    else
+    {
+      int gridX, gridY;
+      gridX = (height + blocksize - 1) / blocksize;
+      gridY = ((width * 3) + blocksize - 1) / blocksize;
+      dim3 dimBlock(blocksize, blocksize);
+      dim3 dimGrid(gridX, gridY);
+      if(kernel_rgb_equal<<<dimGrid, dimBlock>>>(start, end, height, width) == 0) flag_no_change = 1;
+    }
+
     // antimetathese tous pinakes
     temp = start;
     start = end;
